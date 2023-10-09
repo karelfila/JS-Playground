16712783d8f092be7cfa8389d273136cc01f578d
karelfila <72249573+karelfila@users.noreply.github.com>
2023-10-09 17:29:20 +0200

    initial commit

---
+++ playground.js
@@ -0,0 +1,155 @@
+/*
+let firstNumber = 5;
+let secondNumber = 2;
+
+let summary = firstNumber + secondNumber;
+let differential = firstNumber - secondNumber;
+let modulo = firstNumber % secondNumber;
+let product = firstNumber * secondNumber;
+let quotient = firstNumber / secondNumber;
+
+console.log("Součet: " + summary + ", Rozdín: " + differential + ", Modulo: " + modulo + ", Součin: " + product + ", Podíl: " + quotient);
+
+let text = "Typescript Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+
+text = text.replace("Typescript", "Javascript");
+text = text.replaceAll(" ", "");
+doesStartWithJS = text.startsWith("Javascript");
+
+console.log("this text: " + "'" + text + "'" + "\n" + doesStartWithJS + "ly starts with the word Javascript");
+
+
+let vek = 15;
+let plnoletost = vek < 18 ? "neplnoletý" : "plnoletý";  //Ternární operátor
+console.log(plnoletost)
+
+
+let operace = '+'
+let a = 10;
+let b = 5;
+let c;
+
+switch (operace) {
+
+   case '+':
+       c = a + b;
+       break;
+
+   case '-':
+       c = a - b;
+       break;
+
+   case '*':
+       c = a * b;
+       break;
+
+   case '/':
+       c = a / b;
+       break;
+
+   case '%':
+       c = a % b;
+       break;
+}
+
+let isNegative = c < 0 ? "The number is negative" : "The number is positive";  //Ternární operátor
+
+for (let i = 1; i < 9; i++) {
+   console.log("Mám rád JS po " + i + "!")
+}
+
+let nameBackwards = "";
+
+for (let counter = 0; counter < 3; counter++) {
+
+   for (let i = 4; i > -1; i--) {
+       let name = "karel";
+       nameBackwards = nameBackwards + name[i]
+   }
+   console.log(nameBackwards)
+   nameBackwards = "";
+}
+
+const nakup = ['chleba','mkéko', 'sýr', 'nudle', 'káva'];
+
+console.log(nakup.length)
+
+
+console.log("---------------------")
+
+let numbers = [3, 5, 7, 8, 11, 12, 13, 16, 17, 18];
+
+for (let i = 0; i < numbers.length; i++) {
+
+   if(numbers[i] % 2 === 0) {
+       console.log(numbers[i] + " je sudé číslo")
+   }
+
+}
+
+let name = "Karel Fíla";
+
+let names = name.split(" ");
+
+for (let i = 0; i < names.length; i++) {
+
+   console.log(names[i].charAt(0));
+
+}
+
+console.log("---------------------")
+
+function makeLunch() {
+   console.log("🍕");
+   return "🍕";
+}
+
+makeLunch();
+makeLunch();
+
+console.log("---------------------")
+
+
+function minToSec(n) {
+   return n * 60;
+}
+
+console.log(minToSec(3))
+
+
+
+function aZaroven(a, b) {
+   if (a === true && b === true) {
+       return true;
+   } else {
+       return false;
+   }
+}
+
+console.log(aZaroven(true, false));
+
+
+function obsah(x, y) {
+
+   if (x <= 0 || y <= 0) {
+       return -1;
+   } else {
+       return x * y;
+   }
+
+}
+
+console.log(obsah(3, -3));
+
+
+
+//Arrow Funkce
+const makeLunch = () => {
+    return "🍕";
+}
+
+
+const zoo = ["Lev", "Opice", "Tigr"]
+
+zoo.forEach((zvire) => console.log(zvire))
+*/
---
+++ ukol.js
@@ -0,0 +1,82 @@
+const books = [
+    { id: 1, title: 'Hobit', author: 'J.R.R. Tolkien', genre: 'fantasy', price: 250 },
+    { id: 2, title: '1984', author: 'George Orwell', genre: 'sci-fi', price: 180 },
+    { id: 3, title: 'Válka a mír', author: 'Lev Nikolajevič Tolstoj', genre: 'historical', price: 450 },
+    { id: 4, title: 'Kytice', author: 'Karel Jaromír Erben', genre: 'poetry', price: 150 },
+    { id: 5, title: 'Dune', author: 'Frank Herbert', genre: 'sci-fi', price: 280 },
+    { id: 5, title: 'Dune2', author: 'Frank Herbert', genre: 'sci-fi', price: 280 }
+];
+
+
+
+// Vytvořte seznam názvů knih, které stojí více než 200 korun.
+
+let booksThatCostMoreThan200 = books.filter(book => book.price > 200)
+console.log(booksThatCostMoreThan200 = booksThatCostMoreThan200.map(booksThatCostMoreThan200 => booksThatCostMoreThan200.title))
+
+
+
+
+//Vypočtěte průměrnou cenu knihy v kategorii "sci-fi".
+
+function scifiAvg(books) {
+    let priceScifiTotal = 0
+    let counter = 0
+
+    books.forEach(book => {
+        if(book.genre === "sci-fi") {
+            priceScifiTotal += book.price;
+            counter++
+        }
+    })
+    return priceScifiTotal / counter;
+}
+
+console.log(scifiAvg(books))
+
+
+
+
+//Vytvořte nový seznam knih, kde je cena každé knihy snížena o 10%.
+
+let discountedBooks = books.map(book => ({ ...book, price: book.price * 0.9 }));
+
+console.log(discountedBooks)
+
+
+
+
+//Seřazení knih podle ceny od nejvyšší k nejnižší
+
+let booksSortedByPrice = books.slice().sort((a, b) => b.price - a.price);
+
+console.log(booksSortedByPrice)
+
+
+
+
+//Počet knih s více než 3 slovy v názvu
+
+let longTitleBooks = books.filter(book => book.title.split(' ').length > 3).length;
+
+console.log(longTitleBooks)
+
+
+
+
+//Bonus: Vytvořte funkci, která pro danou kategorii vrátí nejdražší knihu.
+
+function findMostExpensiveBookByGenre(genre) {
+
+    let priceTotal = 0
+    let counter = 0
+
+    let genreBooks = books.filter(book => book.genre === genre)
+
+    genreBooks.forEach(genreBook => {
+        priceTotal += genreBook.price;
+        counter++;
+    })
+
+    return priceTotal/counter;
+}
